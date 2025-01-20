8e2c6fd2a14a81ec068437a1636bb828be55f5a9
tiburcioinf <tiburcioinf@yahoo.com.br>
2025-01-20 18:25:51 -0300

    Update cron.py

--- cron.py
+++ cron.py
@@ -3,8 +3,11 @@ from datetime import datetime
 import requests
 
 def loadIMNDData():
+  access_token = 'Basic Y29uY2VpdG86R0dHNiBjaTZzIDdCbm4gSUVQbCAzSXl6IHVYeWo='
+  my_headers = {'Authorization': f'{access_token}'}
   apiURL = f'https://imnd.com.br/api/automation/appointments?page=1&limit=30000&date_start=2024-10-15&date_end=2024-10-30'
-  requisicao = requests.get(urlOFapi, headers=my_headers)
+  
+  requisicao = requests.get(apiURL, headers=my_headers)
 
   if requisicao.status_code == 200:
     data = requisicao.json()
