6899e10fc9d4bba3903089f7ffd54dbae248d02d
tiburcioinf <tiburcioinf@yahoo.com.br>
2025-01-20 18:24:56 -0300

    Update cron.py

--- cron.py
+++ cron.py
@@ -1,8 +1,18 @@
 import time
 from datetime import datetime
+import requests
 
-def minha_tarefa():
+def loadIMNDData():
+  apiURL = f'https://imnd.com.br/api/automation/appointments?page=1&limit=30000&date_start=2024-10-15&date_end=2024-10-30'
+  requisicao = requests.get(urlOFapi, headers=my_headers)
+
+  if requisicao.status_code == 200:
+    data = requisicao.json()
+    print(data)
+  else:
+    print(f"Erro {requisicao.status_code}")
+  
   print(f"Tarefa executada às {datetime.now()}")
 
 if __name__ == "__main__":
-  minha_tarefa()
+  loadIMNDData()
